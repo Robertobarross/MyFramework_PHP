cf6e25c6262c1c41e76bcc0c7a44fda477487602
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-23 22:53:09 -0300

    Update restriction.php

--- config/restriction.php
+++ config/restriction.php
@@ -2,20 +2,24 @@
 // Obs. Incluir em todas as páginas onde é obrigatório estar logado
 session_start(); // Estartando a sessão
 
+if(isset($_POST['nome'])) { // Declarando variáveis da conexão
+    $nome = $_POST['nome'];
+}
+
 if(isset($_POST['email'])) { // Declarando variáveis da conexão
     $email = $_POST['email'];
 }
 
-if(isset($_POST['password'])) { // Declarando variáveis da conexão
-    $password = $_POST['password'];
+if(isset($_POST['senha'])) { // Declarando variáveis da conexão
+    $senha = $_POST['senha'];
 
-    if($email == "email" and $password == "password") { // Se email e password estiveram cadastrados
+    if($nome == "nome" && $email == "email" && $senha /*and*/ == "senha") { // Se email e password estiveram cadastrados
         $_SESSION['login'] =  true; // O login será autorizado
     }
 }
 
 if(!isset($_SESSION['login'])) { // Se o usuário não tiver cadastrado será redirecionado para notAccess
-    header('location:');
+    header('location:/PROJETOS_PHP/MyFramework/errors/notAccess.php');
 }
 
 ?>
