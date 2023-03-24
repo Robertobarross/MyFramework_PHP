e3a713b386f021e3b776895f9a5f6ac409deed87
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-23 23:28:28 -0300

    Delete welcome.php

--- views/welcome.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <h1 align="center">Welcome MyFramework</h1>  
-    <a href="../MyFramework/views/pages/register.php">Register</a>
-    <br>
-    <a href="../MyFramework/views/pages/login.php">Login</a>
-</div>
