9c94b6fe4a4a70aa1472b85ae11a6d383de8c8b5
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-23 22:53:56 -0300

    Create dashboard.php

---
+++ views/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+include('../config/restriction.php');
+include('../config/user.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../public/assets/MyFrameworkPHP.png" type="image/x-icon">
+    <link rel="stylesheet" href="../public/css/styles.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Welcome</h1>  
+    <p><a href="/PROJETOS_PHP/MyFramework/config/logout.php" class="link">LOGOUT</a></p>
+</body>
+</html>
