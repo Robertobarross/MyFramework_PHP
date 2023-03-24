859e6699f5ee582229ef668ba5e5dd68796bd0ca
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-23 22:53:49 -0300

    Update register.php

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,59 +1,32 @@
 <?php
-// Arquivo da conexão com o bd
-include "./database/connect.php";
+   // Conexão com o banco de dados
+   $dsn = 'mysql:host=localhost;dbname=db_myframework_php';
+   $username = 'root';
+   $password = '';
 
-// Declarando variáveis do cadastro
-@$foto = $_FILES['foto']['name'];
-@$tipo = $_FILES['foto']['type'];
-@$usuario = $_POST['usuario'];
-@$email = $_POST['email'];
-@$password = $_POST['password'];
-@$repetepassword = $_POST['repetepassword'];
-@$nivel = $_POST['nivel'];
-
-$cadastro = false; // Para que os campos sejam preenchidos
-if($foto != "" && $usuario != "" && $email != "" && $password != "" && $repetepassword != "" && $nivel != "")
-
-// Confima se password é igual a repetepassword
-if($password != $repetepassword) {
-    echo "<script>alert('Senha não é igual');window.history.go(-1)</script>";
-}else {
-    echo $registro = true; // Se as senhas estiverem corretas o registro é validado 
-    header('location:'); // Permanecer na página de cadstro
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
 }
 
-// Insere as informações na tabela do bd 
-@mysqli_query($link,"INSERT INTO tb_adm(foto,usuario,email,password,repetepassword,nivel)VALUES('$foto','$usuario','$email','$password','$repetepassword','$nivel')");
+// Recebe os dados do formulário
+   $nome = $_POST['nome'];
+   $email = $_POST['email'];
+   $senha = $_POST['senha'];
+   $repeteSenha = $_POST['repeteSenha'];
+
+if ($senha === $repeteSenha) {
+    echo "<script>alert('O usuário foi cadastrado!);window.history.go(-1)</script>";
+    header('location:/PROJETOS_PHP/MyFramework/views/pages/login.php');
+}else{
+   //
+}
+
+   // Insere os dados no banco de dados
+   $stmt = $db->prepare("INSERT INTO users (nome, email, senha) VALUES (?, ?, MD5(?))");
+   $stmt->execute([$nome, $email, $senha]);
+
+   echo "<script>alert('As senhas que você digitol não são iguais!');window.history.go(-1)</script>";
 
 ?>
-
-$host = 'localhost';
-$dbname = 'seu_banco_de_dados';
-$user = 'seu_usuario';
-$password = 'sua_senha';
-
-try {
-  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
-}
-
-$sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
-
-$stmt = $conn->prepare($sql);
-
-$nome = 'Fulano';
-$email = 'fulano@email.com';
-$senha = 'senha123';
-
-$stmt->bindParam(':nome', $nome);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':senha', $senha);
-
-try {
-  $stmt->execute();
-  echo 'Usuário cadastrado com sucesso!';
-} catch(PDOException $e) {
-  echo 'Erro ao cadastrar usuário: ' . $e->getMessage();
-}
