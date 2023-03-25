b7c44660a7974dedf639ad806a142242ce717175
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-24 21:59:58 -0300

    Update logout.php

--- config/logout.php
+++ config/logout.php
@@ -3,5 +3,5 @@
     session_start(); // Inicia a sessão
     session_unset(); // Apaga dados da sessão
     session_destroy(); // Encerra a sessão
-    header('location:/PROJETOS_PHP/MyFramework/index.php'); // Ao encerrar retorna para página inicial
+    header("location: welcome"); // Ao encerrar retorna para página inicial
 ?>
