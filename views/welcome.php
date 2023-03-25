ff7348e05c53c668afd5bbf2b6a636670d747e04
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-24 22:01:08 -0300

    Create welcome.php

---
+++ views/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/css/styles.css">
+    <title>Welcome</title>
+</head>
+<body>
+    <div>
+        <h1 align="center">Welcome MyFramework</h1>  
+        <a href="register">Register</a>
+        <br>
+        <a href="logar">Login</a>
+    </div>    
+</body>
+</html>
