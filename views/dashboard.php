869879dcbe9d68c992cbeb0de8813e6ff4becf36
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-27 22:12:44 -0300

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -7,7 +7,7 @@ include('./config/restriction.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/MyFramework.png" type="image/x-icon">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
     <link rel="stylesheet" href="./public/css/styles.css">
     <title>Dashboard</title>
 </head>
