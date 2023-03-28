e6e2fc837619706af00c946405bdb0ea25298d96
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-27 22:12:11 -0300

    Create not-found.php

---
+++ errors/not-found.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/css/styles.css">
+    <title>404 Not Found</title>
+</head>
+<body>
+    <!-- Textos da página -->
+    <div id="not-found">
+        <p>404 - Not Found</p> 
+        <a href="welcome">RETORNAR</a>
+    </div> 
+</body>
+</html>
