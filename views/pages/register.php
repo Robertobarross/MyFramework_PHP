0be9f6204f677107e2aabce02b4a348cbb6fa547
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-29 23:32:10 -0300

    Update register.php

--- views/pages/register.php
+++ views/pages/register.php
@@ -5,23 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
-    <link rel="stylesheet" href="./public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/myframework.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Register</title>
 </head>
 <body>
-    <h1>Register</h1>
-    <div>
+    <div id="login">
+        <h1 class="titulo-login">Register</h1>
         <form action="registrations" method="POST" enctype="multipart/form-data">
-            <p><input type="text" name="nome" class="campos-form" placeholder="Cadastrar nome do usuário" required></p>
+            <p><input type="text" name="nome" class="form-login" placeholder="Digite seu nome" required></p>
             
-            <p><input type="email" name="email" class="campos-form" placeholder="Cadastrar e-mail" required></p>
+            <p><input type="email" name="email" class="form-login" placeholder="Cadastre seu email" required></p>
         
-            <p><input type="password" name="senha" class="campos-form" placeholder="Cadastrar senha com no máximo 8 caracteres" maxlength="8" required></p>
+            <p><input type="password" name="senha" class="form-login" placeholder="Senha" maxlength="8" required></p>
 
-            <p><input type="password" name="repeteSenha" class="campos-form" placeholder="Cadastrar senha com no máximo 8 caracteres" maxlength="8" required></p>
+            <p><input type="password" name="repeteSenha" class="form-login" placeholder="Repetir senha" maxlength="8" required></p>
 
-            <p><button type="submit" value="cadastrar" class="btn">Cadastrar</button>
-            <input type="reset" value="limpar" class="btn"></p>
+            <p><button type="submit" value="cadastrar" class="btn-login">Register</button>
+            <input type="reset" value="limpar" class="btn-login"></p>
+
+            <a href="login" class="links-login">login</a> |
+            <a href="welcome" class="links-login">Início</a>
         </form>
     </div>   
 </body>
