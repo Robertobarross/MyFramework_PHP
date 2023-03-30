5e9c5c414b5a4f9a048c2cb2f9a566e67633d8d8
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-29 23:32:05 -0300

    Update login.php

--- views/pages/login.php
+++ views/pages/login.php
@@ -5,20 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
-    <link rel="stylesheet" href="./public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/myframework.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <div> 
+    <div id="login"> 
+        <h1 class="titulo-login">Login</h1>
+        <!-- Formulário de login -->
         <form action="logar" method="POST" enctype="multipart/form-data">  
-            <p><input type="email" name="email" class="campos-form" placeholder="Email" required></p>
+            <p><input type="email" name="email" class="form-login" placeholder="Email" required></p>
         
-            <p><input type="password" name="senha" class="campos-form" placeholder="Senha" required></p>
+            <p><input type="password" name="senha" class="form-login" placeholder="Senha" required></p>
 
-            <p><button type="submit" value="cadastrar" class="btn">Logar</button>
-            <input type="reset" value="limpar" class="btn"></p>
+            <p><button type="submit" value="cadastrar" class="btn-login">Logar</button>
+            <input type="reset" value="limpar" class="btn-login"></p>
 
+            <a href="register" class="links-login">Register</a> |
+            <a href="welcome" class="links-login">Início</a> |
+            <a href="#" class="links-login">Esqueci minha senha</a>
         </form>
     </div>  
 </body>
