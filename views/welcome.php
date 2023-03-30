5e077d20249026c30b99d518f2b12cc079ec7a70
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-03-29 23:32:14 -0300

    Update welcome.php

--- views/welcome.php
+++ views/welcome.php
@@ -5,15 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
-    <link rel="stylesheet" href="./public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/myframework.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Welcome</title>
 </head>
 <body>
-    <div>
-        <h1 align="center">Welcome MyFramework</h1>  
-        <a href="register">Register</a>
+    <div id="welcome">
+        <h1 class="titulo">Welcome MyFramework</h1>  
+        
+        <a href="register" class="links">Register</a>
         <br>
-        <a href="login">Login</a>
+        <a href="login" class="links">Login</a>
     </div>    
 </body>
 </html>
