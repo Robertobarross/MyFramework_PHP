1f518603fc95db548a9e39abd57a6e3b01c761fc
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-04-11 23:18:03 -0300

    Create profile.php

---
+++ views/pages/profile.php
@@ -0,0 +1,20 @@
+<?php
+include('./config/restriction.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Perfil</title>
+</head>
+<body>
+    <?php
+     @include('./database/user.php');
+    ?> 
+</body>
+</html>
