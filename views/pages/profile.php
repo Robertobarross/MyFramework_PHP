4cde05eb0ae87d44f13495110d9a7dabaf9af873
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-04-17 22:53:48 -0300

    Update profile.php

--- views/pages/profile.php
+++ views/pages/profile.php
@@ -8,13 +8,14 @@ include('./config/restriction.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/css/myframework.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
     <title>Perfil</title>
 </head>
 <body>
-    <?php
-     @include('./database/user.php');
-    ?> 
+        <?php
+          @include('./views/template.php'); // Template
+          @include('./database/user.php'); // Editar usuário logado
+        ?>
 </body>
 </html>
