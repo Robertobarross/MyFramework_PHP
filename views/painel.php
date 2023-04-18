36b4c71ce1778b2538322674ef929e1730a9281c
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-04-17 22:53:55 -0300

    Create painel.php

---
+++ views/painel.php
@@ -0,0 +1,22 @@
+<?php
+include('./config/restriction.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/css/myframework.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Painel</title>
+</head>
+<body>    
+    <?php
+       @include('template.php');
+    ?>
+    
+    <h1 class="titulo">PAINEL</h1> 
+</body>
+</html>
