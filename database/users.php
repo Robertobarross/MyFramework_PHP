185e8b803509d5861677884e723b7ed7a073ec37
Roberto Barrosss <robertobarros27esp@gmail.com>
2023-05-01 21:44:25 -0300

    Update users.php

--- database/users.php
+++ database/users.php
@@ -1,24 +1,61 @@
-<?php
-// Retorna todos os registros do banco de dados
-include('connect.php');
-include('./config/restriction.php');
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./public/assets/MyFramework.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/css/myframework.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Nível de acesso</title>
+</head>
+<body>
+        <?php
+        include('./views/template.php');
+        ?>
+        
+        <br>
+        <h1 class="text-adm">Pesquisar usuários para conceder nível de acesso</h1>
+        <p class="text-adm">Pesquise o usuário que deseja encontrar pelo endereço de email ou nome</p>
 
-// Suponha que o ID do usuário logado seja armazenado na variável $user_id
-$stmt = $conn->prepare("SELECT * FROM users WHERE id = :id");
-$stmt->bindParam(':id', $user_id);
-$stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+        <?php // Retorna a mensagem confirmando a atulização do nível
+           $mensagemDeRetorno = isset($_SESSION['msg-nivel']) ? $_SESSION['msg-nivel'] : '';
+           if(!empty($mensagemDeRetorno))
+           {
+               echo $mensagemDeRetorno;
+           }
+        ?>
+
+        <br>
+        <form action="" method="post" class="users-search">
+		<input type="text" name="email" class="users-search" placeholder="Pesquisar" maxlength="200">
+		<input type="submit" value="Ir">
+	</form>
+        <hr>
+        
+        <?php
+          include('./config/restriction.php');
+          include('./config/nivel-adm.php');
+          include('editNivel.php');
+        ?>
+</body>
+</html>
+
+<style>
+.text-adm{
+        font-size: 17px;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-left: 17%;
 
 
-
-$select = $conn->query("SELECT * FROM users WHERE id");
-$data = $select->fetchAll();
-foreach ($data as $row){
-        echo '<tr class="text-center">';
-        echo '<td>'.$row["nome"].'</td>';
-        echo '<td>'.$row["email"].'</td>';
-        echo '<td><a href="profile.php?link=3&id='.$row["id"].'" class="btn btn-primary">'.'EDITAR'.'</a> <a href="profile.php?link=4&id='.$row["id"].'" class="btn btn-danger">'.'DELETAR'.'</a> </td>';
-        echo '</tr>';
 }
+.users-search{
+        width: 50%;
+        height: auto;
+        margin-left: 4%;
+        font-size: 12px;
+        font-family: Arial, Helvetica, sans-serif;
+        color: #808080;
 
-?>
+}
+</style>
